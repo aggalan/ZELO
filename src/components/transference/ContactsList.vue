9f0582a35d6df1772e5e6398a1fb58ab7ac449ba
smaffeo <smaffeo@itba.edu.ar>
2024-10-24 17:19:42 -0300

    transfer page better

--- src/components/transference/ContactsList.vue
+++ src/components/transference/ContactsList.vue
@@ -3,6 +3,7 @@
     <slot/>
     <v-card-text class="pa-4">
       <v-text-field
+        v-model="search"
         prepend-inner-icon="mdi-magnify"
         label="Buscar"
         single-line
@@ -12,7 +13,7 @@
         density="comfortable"
       ></v-text-field>
       <v-list class="rounded-lg bg-grey-lighten-4">
-        <v-list-item v-for="contact in contacts" :key="contact.name" class="py-2">
+        <v-list-item v-for="contact in filteredContacts" :key="contact.name" class="py-2">
           <template v-slot:prepend>
             <v-avatar :color="contact.color || 'grey-lighten-2'" size="40">
               <span class="text-h6 white--text">{{ contact.avatar }}</span>
@@ -20,7 +21,7 @@
           </template>
           <v-list-item-title class="font-weight-medium">{{ contact.name }}</v-list-item-title>
           <template v-slot:append>
-            <ActionButton  customClass="text-none px-6">Transferir</ActionButton>
+            <ActionButton @click="$emit('complete-transfer', contact)" customClass="text-none px-6">Transferir</ActionButton>
           </template>
         </v-list-item>
       </v-list>
@@ -30,22 +31,19 @@
 
 <script setup>
 import { ref, computed } from 'vue'
-import ActionButton from "@/components/generalComponents/ActionButton.vue";
+import ActionButton from "@/components/generalComponents/ActionButton.vue"
+
 const props = defineProps({
-  contacts: { type: Array, default: [] }
+  contacts: { type: Array, default: () => [] }
 })
+
+const emit = defineEmits(['complete-transfer'])
 
 const search = ref('')
 
-
 const filteredContacts = computed(() => {
-  return contacts.value.filter(contact =>
+  return props.contacts.filter(contact =>
     contact.name.toLowerCase().includes(search.value.toLowerCase())
   )
 })
-
-const completeTransfer = (contact) => {
-  // Handle completing transfer with contact
-  console.log('Completing transfer for:', contact.name)
-}
 </script>
