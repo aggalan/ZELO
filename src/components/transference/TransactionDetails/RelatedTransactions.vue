cefccd2d60c221942774b339017f95fa7f048342
Jose Benegas Lynch <jobenegaslynch@itba.edu.ar>
2024-10-25 16:10:11 -0300

    lol

--- src/components/transference/TransactionDetails/RelatedTransactions.vue
+++ src/components/transference/TransactionDetails/RelatedTransactions.vue
@@ -4,21 +4,21 @@
     <v-list two-line>
       <v-list-item v-for="transaction in transactions" :key="transaction.id" class="py-3">
         <v-list-item-avatar>
-          <v-icon :color="transaction.type === 'incoming' ? 'success' : 'error'" size="32">
-            {{ transaction.type === 'incoming' ? 'mdi-arrow-down-circle' : 'mdi-arrow-up-circle' }}
+          <v-icon :color="transaction.type === 'ingreso' ? 'success' : 'error'" size="32">
+            {{ transaction.type === 'ingreso' ? 'mdi-arrow-down-circle' : 'mdi-arrow-up-circle' }}
           </v-icon>
         </v-list-item-avatar>
         <v-list-item-content>
           <v-list-item-title class="font-weight-medium">
-            {{ transaction.type === 'incoming' ? transaction.sender : transaction.recipient }}
+            {{ transaction.to }}
           </v-list-item-title>
           <v-list-item-subtitle>
-            {{ formatDate(transaction.date) }}
+            {{ transaction.time }}
           </v-list-item-subtitle>
         </v-list-item-content>
         <v-list-item-action>
-          <v-list-item-action-text :class="transaction.type === 'incoming' ? 'success--text' : 'error--text'">
-            <span class="text-h6 font-weight-bold">{{ transaction.type === 'incoming' ? '+' : '-' }}${{ transaction.amount.toFixed(2) }}</span>
+          <v-list-item-action-text :class="transaction.type === 'ingreso' ? 'success--text' : 'error--text'">
+            <span class="text-h6 font-weight-bold">{{ transaction.type === 'ingreso' ? '+' : '-' }}${{ transaction.amount.toFixed(2) }}</span>
           </v-list-item-action-text>
         </v-list-item-action>
       </v-list-item>
@@ -35,9 +35,4 @@ const props = defineProps({
     required: true
   }
 })
-
-const formatDate = (dateString) => {
-  const options = { year: 'numeric', month: 'short', day: 'numeric' }
-  return new Date(dateString).toLocaleDateString('es-ES', options)
-}
 </script>
